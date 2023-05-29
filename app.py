3bedfabdfa939f5a7f8cf45dfcda9b348778ce97
AndrewSushi <AndrewSushi8@gmail.com>
2023-05-28 21:37:50 -0700

    Auto commit

--- app.py
+++ app.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/handle')
+def submit():
+    todo = request.args["action"]
+    return render_template('form_submit.html', todo=todo)
